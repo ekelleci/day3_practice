ff9969edd1926c74d93262eee54887d65c50fa9d
ekelleci <ekelleci@outlook.com>
2022-01-18 10:46:40 -0500

    step 2

--- src/day3_project1.java
+++ src/day3_project1.java
@@ -2,6 +2,6 @@ public class day3_project1 {
     public static void main(String[] args) {
         System.out.println("Wooden spoon");
         // step 1
-        //step 2
+        //step 2.
     }
 }
